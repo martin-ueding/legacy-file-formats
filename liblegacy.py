80c60ce05b87b2ff605bce8e8aed082f4ac6782d
Martin Ueding <dev@martin-ueding.de>
2012-01-08 11:24:18 +0100

    Refactor into smaller functions

--- liblegacy.py
+++ liblegacy.py
@@ -45,42 +45,57 @@ def checkfolder(args, dirname, names):
                     # This is the standard export file name.
                     exportfile = dirname+"/"+name+"."+exportsuffix
 
-                    # Check whether the file has the same name, but just a
-                    # export extension.  Rename the file then.
-                    if not os.path.isfile(exportfile):
-                        alt_exportfile = dirname+"/"+os.path.splitext(name)[0]+"."+exportsuffix
-                        if os.path.isfile(alt_exportfile):
-                            if options.rename:
-                                os.rename(alt_exportfile, exportfile)
-                                if options.verbose:
-                                    print "Renaming", alt_exportfile, exportfile
-
-                            else:
-                                if options.verbose:
-                                    print "Would rename", alt_exportfile, exportfile
+                    check_rename(dirname, name, exportfile, exportsuffix, options)
 
                     # Check for the file again. This time, see whether its
                     # modification time is newer than the original, if that
                     # option is specified.
                     if os.path.isfile(exportfile):
                         if options.time:
-                            origtime = os.path.getmtime(dirname+"/"+name)
-                            exporttime = os.path.getmtime(exportfile)
-
-                            # If the export is newer than the origtime, the
-                            # file is valid.
-                            if exporttime > origtime:
+                            if not check_time(dirname+"/"+name, exportfile):
                                 is_invalid = False
                         else:
                             is_invalid = False
 
                 if is_invalid:
-                    print dirname+"/"+name
+                    mark_invalid(dirname, name, pattern, counts)
 
-                    if not pattern in counts:
-                        counts[pattern] = 0
 
-                    counts[pattern] += 1
+def check_time(origfile, exportfile):
+    origtime = os.path.getmtime(origfile)
+    exporttime = os.path.getmtime(exportfile)
+
+    # If the export is newer than the origtime, the
+    # file is valid.
+    return exporttime > origtime
+
+
+def check_rename(dirname, name, exportfile, exportsuffix, options):
+    """
+    Check whether the file has the same name, but just a export extension.
+
+    Rename the file then.
+    """
+    if not os.path.isfile(exportfile):
+        alt_exportfile = dirname+"/"+os.path.splitext(name)[0]+"."+exportsuffix
+        if os.path.isfile(alt_exportfile):
+            if options.rename:
+                os.rename(alt_exportfile, exportfile)
+                if options.verbose:
+                    print "Renaming", alt_exportfile, exportfile
+
+            else:
+                if options.verbose:
+                    print "Would rename", alt_exportfile, exportfile
+
+
+def mark_invalid(dirname, name, pattern, counts):
+    print dirname+"/"+name
+
+    if not pattern in counts:
+        counts[pattern] = 0
+
+    counts[pattern] += 1
 
 
 def print_summary(counts):
