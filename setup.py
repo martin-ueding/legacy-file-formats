08ae3694a99ce1521cf3d135f11be8f8c7b758d7
Martin Ueding <dev@martin-ueding.de>
2012-01-28 18:15:06 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    name = "legacylib",
+    author = "Martin Ueding",
+    author_email = "dev@martin-ueding.de",
+    license = "GPL3",
+    version = "1.4",
+
+    packages = ["legacylib"],
+)
