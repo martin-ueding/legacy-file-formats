39ec09e846c74eb7fc5d3f6938556b134fbbc6d7
Martin Ueding <dev@martin-ueding.de>
2014-02-17 19:10:50 +0100

    Change setup.py to setuptools

--- setup.py
+++ setup.py
@@ -3,17 +3,24 @@
 
 # Copyright © 2012-2014 Martin Ueding <dev@martin-ueding.de>
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     author = "Martin Ueding",
     author_email = "dev@martin-ueding.de",
     description = "Finds file formats that might become unreadable.",
     license = "GPL3",
-    name = "legacylib",
-    packages = ["legacylib"],
-    requires = ["prettytable"],
-    scripts = ["legacy"],
+    name = "legacyfileformats",
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'legacy-file-formats = legacyfileformats:main',
+        ],
+    },
+    install_requires=[
+        'prettytable',
+        'termcolor',
+    ],
     url = "https://github.com/martin-ueding/legacy-file-formats",
     version = "1.6.1",
 )
